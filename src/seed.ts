14d8816f1e7a431964dc1ebba1668dcc03e7b504
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-09-17 13:40:03 +0800

    Create Login Admin and Generate Token

--- src/seed.ts
+++ src/seed.ts
@@ -1,5 +1,9 @@
 import prisma from "./utils/client";
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 // CREATE A USER
 async function crateAdminUser() { 
@@ -7,14 +11,63 @@ async function crateAdminUser() {
     const hashPassword = bcrypt.hashSync("@lunas123", 10);
     await prisma.admin.create({
         data: {
-            firstname: "Danielle",
-            lastname: "Lunas",
-            email: 'lunas.danielle.10262002@gmail.com',
+            firstname: "Testing",
+            lastname: "Testing",
+            email: 'testing@gmail.com',
             password: hashPassword,
         },
     });
 
     console.log('Database Seeded Successfully.');
+}
+
+// LOGIN ADMIN USER
+async function loginAdminUser() {
+
+    try {
+        
+        const email = "testing@gmail.com";
+        const password = "@lunas123";
+
+        const validateAdminUser = await prisma.admin.findUnique({
+            where: { 
+                email:email   
+            }
+        });
+
+        if (!validateAdminUser) {
+            console.log("Admin Not Found!");
+            return;
+        }
+
+        const isPasswordValid = bcrypt.compareSync(password, validateAdminUser.password);
+        if (!isPasswordValid) {
+            console.log("Invalid Password");
+            return;
+        }
+
+        //
+        if (!process.env.JWT_SECRET_KEY) {
+            throw new Error("JWT_SECRET_KEY Is Not Defined in Environment Variables.");
+        }
+
+        // Create JWT for the user
+        const token = jwt.sign(
+            { 
+                id: validateAdminUser.id, 
+                email: validateAdminUser.email 
+            }, 
+            process.env.JWT_SECRET_KEY, 
+            { expiresIn: '1h' }
+        );
+
+        console.log("Login Successfull. JWT Token:", token);
+        return token;
+
+    } catch (error) {
+        console.error(error);
+    }
+
 }
 
 
@@ -67,34 +120,43 @@ async function deleteAdminUser() {
     console.log(deletedAdminUser);
 }
 
-crateAdminUser()
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+// crateAdminUser()
+//     .catch(error => {
+//         console.error(error);
+//         process.exit(1);
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect();
+//     });
 
-getAllAdminUserss()
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+// getAllAdminUserss()
+//     .catch(error => {
+//         console.error(error);
+//         process.exit(1);
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect();
+//     });
 
-updateAdminUser()
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+// updateAdminUser()
+//     .catch(error => {
+//         console.error(error);
+//         process.exit(1);
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect();
+//     });
 
-deleteAdminUser()
+// deleteAdminUser()
+//     .catch(error => {
+//         console.error(error);
+//         process.exit(1);
+//     })
+//     .finally(async () => {
+//         await prisma.$disconnect();
+//     });
+
+loginAdminUser()
     .catch(error => {
         console.error(error);
         process.exit(1);
