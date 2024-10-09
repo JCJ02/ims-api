88b2fef3450e3e9751d46f78811d78336276c3cc
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-09 17:32:44 +0800

    Feat: Add Search Roles Route

--- src/routes/RoleRoute.ts
+++ src/routes/RoleRoute.ts
@@ -8,6 +8,7 @@ roleRoute.post("/create", roleController.createRole);
 roleRoute.put("/update-role/:id", roleController.updateRole);
 roleRoute.put("/delete-role/:id", roleController.deleteRole);
 roleRoute.get("/get-roles", roleController.getRoles);
+roleRoute.get("/get-roles/search", roleController.searchRoles);
 
 export default roleRoute;
 
