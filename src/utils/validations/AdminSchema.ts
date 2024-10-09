7b0d4f281ddfd6bc8a1c2fb73e6b3804b5a4e62e
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-09 15:08:15 +0800

    Fix: Admin Validations

--- src/utils/validations/AdminSchema.ts
+++ src/utils/validations/AdminSchema.ts
@@ -3,13 +3,13 @@ import { z } from "zod";
 const adminSchema = z.object({
     firstname: z.string({
         required_error: "Firstname Is Required!",
-        invalid_type_error: "Firstname Must Be A String!"
+        invalid_type_error: "Firstname Must Be String!"
     })
         .min(3, "Firstname Must Be At Least 3 Characters Long!")
         .max(255, "Firstname Must Not Exceed 255 Characters!"),
     lastname: z.string({
     required_error: "Firstname Is Required!",
-        invalid_type_error: "Firstname Must Be A String!"
+        invalid_type_error: "Firstname Must Be String!"
     })
         .min(3, "Firstname Must Be At Least 3 Charaters Long!")
         .max(255, "Firstname Must Not Exceed 255 Characters!"),
@@ -28,13 +28,13 @@ const adminSchema = z.object({
 const updateAdminSchema = z.object({
     firstname: z.string({
         required_error: "Firstname Is Required!",
-        invalid_type_error: "Firstname Must Be A String!"
+        invalid_type_error: "Firstname Must Be String!"
     })
         .min(3, "Firstname Must Be At Least 3 Characters Long!")
         .max(255, "Firstname Must Not Exceed 255 Characters!"),
     lastname: z.string({
     required_error: "Firstname Is Required!",
-        invalid_type_error: "Firstname Must Be A String!"
+        invalid_type_error: "Firstname Must Be String!"
     })
         .min(3, "Firstname Must Be At Least 3 Charaters Long!")
         .max(255, "Firstname Must Not Exceed 255 Characters!"),
