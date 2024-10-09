17a871282c23a0c0501585151627a90014f26fe4
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-09 15:05:09 +0800

    Feat: Add Role Route from RoleRoute.ts file

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import adminRoute from "./AdminRoute";
+import roleRoute from "./RoleRoute";
 
 const routes = express.Router();
 
 routes.use("/admin", adminRoute);
+routes.use("/role", roleRoute);
 
 export default routes;
 
