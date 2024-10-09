0dc1ebf723b850b2f618de1143a5b34237ad920f
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-09 14:37:05 +0800

    Feat: Add Create a Role

--- src/seed.ts
+++ src/seed.ts
@@ -1,35 +1,76 @@
 import prisma from "./utils/client";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 
-// CREATE A USER
-async function crateAdminUser() { 
+// CREATE AN ADMIN USER
+async function createAdmin() { 
 
     try {
+
         const hashPassword = bcrypt.hashSync("password", 10);
-        await prisma.admin.create({
-            data: {
-                firstname: "Danielle",
-                lastname: "Lunas",
-                email: 'lunas.danielle.10262002@gmail.com',
-                password: hashPassword,
-                role: "Admin"
-            },
+
+        await prisma.$transaction(async (prisma) => {
+            await prisma.admin.create({
+                data: {
+                    firstname: "Danielle",
+                    lastname: "Lunas",
+                    email: 'lunas.danielle.10262002@gmail.com',
+                    account: {
+                        create: {
+                            password: hashPassword
+                        }
+                    }
+                },
+            });
         });
 
-        console.log('Database Seeded Successfully.');
+        console.log('Successfully Registered!.');
+
     } catch (error) {
-        console.error(error);
+
+        console.error("Error Seeding Database: ", error);
+
+    } finally {
+
+        await prisma.$disconnect();
+
+    }
+    
+}
+
+async function createRole() {
+
+    try {
+        
+        await prisma.role.create({
+            data: {
+                roleId: "24-0001",
+                roleName: "Admin",
+                roleDescription: "Adminstrator"
+            }
+        });
+
+        console.log("Successfully Created!")
+
+    } catch (error) {
+        
+        console.error("ERROR Seeding Database: ", error)
+
+    } finally {
+
+        await prisma.$disconnect();
+
     }
 
 }
 
-crateAdminUser()
+createAdmin()
     .catch(error => {
-        console.error(error);
+        console.error("Error In Admin User Creation: ", error);
         process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
     });
 
+createRole()
+    .catch(error => {
+        console.error("Error In Role Creation: ", error);
+        process.exit(1);
+    });
