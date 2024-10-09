de53c76a80122f358322525c789cfff29d4289f8
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-09 15:03:43 +0800

    Fix: Bugs in Create, Update Admin & Admin Pass, and Get Admins

--- src/repo/AdminRepo.ts
+++ src/repo/AdminRepo.ts
@@ -2,27 +2,38 @@ import prisma from "../utils/client";
 
 class AdminRepo {
 
+    // CREATE ADMIN METHOD
     async createAdmin(data: any) {
-            
-        const newAdmin = await prisma.admin.create({
-            data: {
-                firstname: data.firstname,
-                lastname: data.lastname,
-                email: data.email,
-                password: data.password,
-            },
+
+        const newAdmin = await prisma.$transaction(async (prisma) => {
+            return await prisma.admin.create({
+                data: {
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    email: data.email,
+                    account: {
+                        create: {
+                            password: data.password
+                        }
+                    }
+                },
+            });
         });
 
         return newAdmin;
 
     }
 
+    // AUTHENTICATE OR LOGIN ADMIN METHOD
     async authenticate(data: any) {
         //console.log("Email: ", data.email);
         const admin = await prisma.admin.findFirst({
             where: {
                 email: data.email,
                 deletedAt: null
+            },
+            include: {
+                account: true
             }
         });
         //console.log("Admin: ", admin);
@@ -30,12 +41,16 @@ class AdminRepo {
 
     }
 
+    // GET ADMIN ID METHOD
     async show(id: number) {
 
-        const adminId = await prisma.admin.findFirst({
+        const adminId = await prisma.admin.findUnique({
             where: {
                 id: id,
                 deletedAt: null
+            },
+            include: {
+                account: true
             }
         });
 
@@ -43,7 +58,7 @@ class AdminRepo {
 
     }
 
-    // UPDATE ADMIN
+    // UPDATE ADMIN METHOD
     async updateAdmin(data: any) {
 
         const editAdmin = await prisma.admin.update({
@@ -61,22 +76,33 @@ class AdminRepo {
 
     }
 
-    // UPDATE ADMIN PASSWORD
-    async updateAdminPassword(id: number, data: { newPassword: string, currentPassword: string }) {
+    // UPDATE ADMIN PASSWORD METHOD
+    async updateAdminPassword(id: number, data: { newPassword: string }) {
 
-        const editAdminPassword = await prisma.admin.update({
+        const editAdminPassword = await prisma.account.update({
             where: { 
                 id: id, 
             },
-            data: { password: data.newPassword }
+            data: {
+                password: data.newPassword
+            }
         });
 
         return editAdminPassword;
 
     }
 
-    async getAdmin(data: any) {
+    // GET ALL ADMIN DATA METHOD
+    async getAllAdmin(data: any) {
+        
+        const getAllAdmin = await prisma.admin.findMany({
+            where: {
+                id: data.id,
+                deletedAt: null
+            }
+        });
 
+        return getAllAdmin;
 
     }
 
