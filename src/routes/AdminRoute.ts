024ad59e1b859b5cecd721150ce2b1eb982442d5
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-09 15:06:46 +0800

    Feat: Add Create, Update, Get, and Auth Routes for Admin

--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -1,11 +1,12 @@
-import express from "express"
+import express from "express";
 import AdminController from "../controllers/AdminController";
 import authAdminMiddleware from "../middlewares/AuthAdminMiddleware";
 
 const adminRoute = express.Router();
 const adminController = new AdminController();
 
-adminRoute.get("/test", authAdminMiddleware, adminController.index);
+adminRoute.get("/index", authAdminMiddleware, adminController.index);
+adminRoute.get("/", adminController.getAllAdmin);
 adminRoute.post("/create", adminController.createAdmin);
 adminRoute.post("/authenticate", adminController.authenticate);
 adminRoute.put("/update/:id", adminController.updateAdmin);
