05e9f57d54654a46f90478b7fcc4a0c8736198bf
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-21 17:54:45 +0800

    Feat: Add Intern and Mentor API for Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 import adminRoute from "./AdminRoute";
 import roleRoute from "./RoleRoute";
+import mentorRoute from "./MentorRoute";
+import internRoute from "./InternRoute";
 
 const routes = express.Router();
 
 routes.use("/admin", adminRoute);
 routes.use("/role", roleRoute);
+routes.use("/mentor", mentorRoute);
+routes.use("/intern", internRoute);
 
 export default routes;
 
