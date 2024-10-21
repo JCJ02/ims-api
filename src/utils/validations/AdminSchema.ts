336cbc1c1ad5a9afb29e5081c72a5067c1b33a12
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-21 21:08:28 +0800

    Fix: Bugs

--- src/utils/validations/AdminSchema.ts
+++ src/utils/validations/AdminSchema.ts
@@ -8,7 +8,7 @@ const adminSchema = z.object({
         .min(3, "Firstname Must Be At Least 3 Characters Long!")
         .max(255, "Firstname Must Not Exceed 255 Characters!"),
     lastname: z.string({
-    required_error: "Firstname Is Required!",
+        required_error: "Firstname Is Required!",
         invalid_type_error: "Firstname Must Be String!"
     })
         .min(3, "Firstname Must Be At Least 3 Charaters Long!")
