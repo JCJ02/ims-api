b6f4a8a7e4f6b872119562276d7498d42284a691
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-22 23:15:01 +0800

    Feat: Create, Update, and Delete API

--- src/repo/MentorRepo.ts
+++ src/repo/MentorRepo.ts
@@ -1,6 +1,75 @@
+import { mentorType } from "../types/MentorType";
 import prisma from "../utils/client";
 
 class MentorRepo {
+
+    // CREATE MENTOR METHOD
+    async createMentor(data: mentorType) {
+
+        const newMentor = await prisma.$transaction( async (prisma) => {
+            return await prisma.mentor.create({
+                data: {
+                    firstname: data.firstname,
+                    lastname: data.lastname,
+                    email: data.email,
+                    role: data.role
+                }
+            });
+        });
+
+        return newMentor;
+
+    }
+
+    // SHOW METHOD
+    async show(id: number) {
+
+        const mentorId = await prisma.mentor.findFirst({
+            where: {
+                id: id,
+                deletedAt: null
+            }
+        });
+
+        return mentorId;
+
+    }
+
+    // UPDATE MENTOR METHOD
+    async updateMentor(id: number, data: mentorType) {
+        
+        const updateMentorData = await prisma.mentor.update({
+            where: {
+                id: id
+            },
+            data: {
+                firstname: data.firstname,
+                lastname: data.lastname,
+                email: data.email,
+                role: data.role
+            }
+        });
+
+        return updateMentorData;
+
+    }
+
+    // SOFT DELETE MENTOR METHOD
+    async deleteMentor(id: number) {
+
+        const softDeleteMentor = await prisma.mentor.update({
+            where: {
+                id: id,
+                deletedAt: null
+            },
+            data: {
+                deletedAt: new Date()
+            }
+        });
+
+        return softDeleteMentor;
+
+    }
 
 }
 
--- src/services/MentorService.ts
+++ src/services/MentorService.ts
@@ -1,7 +1,65 @@
+import MentorRepo from "../repo/MentorRepo";
+import { mentorType } from "../types/MentorType";
+
 class MentorService {
+
+    private mentorRepo;
 
     constructor() {
         
+        this.mentorRepo = new MentorRepo();
+        
+    }
+
+    // CREATE MENTOR METHOD
+    async createMentor(data: mentorType) {
+        
+        const mentorData = {
+            ...data
+        }
+
+        const newMentor = await this.mentorRepo.createMentor(mentorData);
+
+        return newMentor;
+
+    }
+
+    // UPDATE MENTOR METHOD
+    async updateMentor(id: number, data: mentorType) {
+
+        const mentor = await this.mentorRepo.show(id);
+
+        if(!mentor) {
+            return null;
+        } else {
+
+            const mentorData = {
+                ...data
+            }
+
+            const updateMentorData = this.mentorRepo.updateMentor(mentor.id, mentorData);
+
+            return updateMentorData;
+
+        }
+
+    }
+
+    // SOFT DELETE MENTOR METHOD
+    async deleteMentor(id: number) {
+
+        const mentor = await this.mentorRepo.show(id);
+
+        if(!mentor) {
+            return null;
+        } else {
+
+            const deleteMentor = await this.mentorRepo.deleteMentor(mentor.id);
+
+            return deleteMentor;
+
+        }
+
     }
 
 }
