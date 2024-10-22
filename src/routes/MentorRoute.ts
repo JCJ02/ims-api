dd10889dfab544081fa4bc497f550084a8b1d82e
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-22 23:15:27 +0800

    Feat: Add Mentor Routes

--- src/routes/MentorRoute.ts
+++ src/routes/MentorRoute.ts
@@ -4,5 +4,8 @@ import MentorController from "../controllers/MentorController";
 const mentorRoute = express.Router();
 const mentorController = new MentorController();
 
+mentorRoute.post("/create", mentorController.createMentor);
+mentorRoute.put("/update/:id", mentorController.updateMentor);
+mentorRoute.put("/delete/:id", mentorController.deleteMentor);
 
 export default mentorRoute;
