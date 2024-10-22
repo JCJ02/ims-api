9c5a0d3223ea0ad760c5d90a7f80f34de8f96383
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-22 17:22:01 +0800

    Feat: Add Mentor Routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import adminRoute from "./AdminRoute";
 import roleRoute from "./RoleRoute";
-import mentorRoute from "./MentorRoute";
 import internRoute from "./InternRoute";
+import mentorRoute from "./MentorRoute";
 
 const routes = express.Router();
 
@@ -10,6 +10,7 @@ routes.use("/admin", adminRoute);
 routes.use("/role", roleRoute);
 routes.use("/mentor", mentorRoute);
 routes.use("/intern", internRoute);
+routes.use("/mentor", mentorRoute);
 
 export default routes;
 
