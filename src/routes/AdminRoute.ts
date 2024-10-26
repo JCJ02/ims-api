4990318e367fd04407de56e11c30105efaaac11e
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-10-27 00:53:38 +0800

    Fix: Polise the codes

--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -14,7 +14,7 @@ adminRoute.put("/update/:id", adminController.updateAdmin);
 adminRoute.post("/update-password/:id", adminController.updateAdminPassword);
 adminRoute.put("/delete/:id", adminController.deleteAdmin);
 adminRoute.post("/send-email", adminController.sendEmail);
-adminRoute.get("/get-admins", adminController.getAdmins);
+adminRoute.get("/get-admins", adminController.getAdminsList);
 adminRoute.get("/get-admins/search", adminController.searchAdmins);
 
 export default adminRoute;
