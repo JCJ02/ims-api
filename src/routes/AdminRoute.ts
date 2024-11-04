57b820bb4f62e6ac56f990cc404aea3e5dd73816
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-04 11:54:56 +0800

    Fix: Routes Names

--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -1,19 +1,19 @@
 import express from "express";
 import AdminController from "../controllers/AdminController";
-import authMiddleware from "../middlewares/AuthMiddleware";
+import authMiddleware from "../middleware/AuthMiddleware";
 
 const adminRoute = express.Router();
 const adminController = new AdminController();
 
 adminRoute.get("/test", adminController.test);
-adminRoute.get("/index", authMiddleware, adminController.index);
-adminRoute.post("/create", adminController.createAdmin);
+adminRoute.get("/", authMiddleware, adminController.dashboard);
+adminRoute.post("/", authMiddleware, adminController.create);
 adminRoute.post("/authenticate", adminController.authenticate);
-adminRoute.put("/update/:id", adminController.updateAdmin);
-adminRoute.post("/update-password/:id", adminController.updateAdminPassword);
-adminRoute.put("/delete/:id", adminController.deleteAdmin);
-adminRoute.post("/send-email", adminController.sendEmail);
-adminRoute.get("/get-admins", adminController.getAdminsList);
-adminRoute.get("/get-admins/search", adminController.searchAdmins);
+adminRoute.put("/:id", authMiddleware, adminController.update);
+adminRoute.put("/update-password/:id", authMiddleware, adminController.updatePassword);
+adminRoute.delete("/:id", authMiddleware, adminController.delete);
+adminRoute.post("/send-email", authMiddleware, adminController.sendEmail);
+adminRoute.get("/list", authMiddleware, adminController.list);
+adminRoute.get("/:id", authMiddleware, adminController.get);
 
 export default adminRoute;
