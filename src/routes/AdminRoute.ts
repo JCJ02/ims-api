e7e9d80184f7e1675dfba24e1ce6e5204c278f6b
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-04 20:30:02 +0800

    Fix: Routes Endpoints

--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -7,7 +7,7 @@ const adminController = new AdminController();
 
 adminRoute.get("/test", adminController.test);
 adminRoute.get("/dashboard", authMiddleware, adminController.dashboard);
-adminRoute.post("/", adminController.create);
+adminRoute.post("/", authMiddleware, adminController.create);
 adminRoute.post("/authenticate", adminController.authenticate);
 adminRoute.put("/:id", authMiddleware, adminController.update);
 adminRoute.put("/update-password/:id", authMiddleware, adminController.updatePassword);
