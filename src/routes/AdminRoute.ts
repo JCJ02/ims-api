8ae91502c8ab281bd7533c94195506e05ace359e
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2024-11-04 19:15:55 +0800

    Fix: Adjust Routes

--- src/routes/AdminRoute.ts
+++ src/routes/AdminRoute.ts
@@ -6,15 +6,14 @@ const adminRoute = express.Router();
 const adminController = new AdminController();
 
 adminRoute.get("/test", adminController.test);
-
-adminRoute.get("/index", authMiddleware, adminController.index);
-adminRoute.post("/create", adminController.createAdmin);
+adminRoute.get("/dashboard", authMiddleware, adminController.dashboard);
+adminRoute.post("/", adminController.create);
 adminRoute.post("/authenticate", adminController.authenticate);
 adminRoute.put("/:id", authMiddleware, adminController.update);
 adminRoute.put("/update-password/:id", authMiddleware, adminController.updatePassword);
 adminRoute.delete("/:id", authMiddleware, adminController.delete);
 adminRoute.post("/send-email", authMiddleware, adminController.sendEmail);
-adminRoute.get("/list", authMiddleware, adminController.list);
+adminRoute.get("/", authMiddleware, adminController.list);
 adminRoute.get("/:id", authMiddleware, adminController.get);
 
 export default adminRoute;
--- src/routes/InternRoute.ts
+++ src/routes/InternRoute.ts
@@ -5,13 +5,13 @@ import authMiddleware from "../middleware/AuthMiddleware";
 const internRoute = express.Router();
 const internController = new InternController();
 
-internRoute.get("/", authMiddleware, internController.dashboard);
+internRoute.get("/dashboard", authMiddleware, internController.dashboard);
 internRoute.post("/", authMiddleware, internController.create);
 internRoute.post("/authenticate", internController.authenticate);
 internRoute.put("/:id", authMiddleware, internController.update);
 internRoute.put("/update-password/:id", authMiddleware, internController.updatePassword);
 internRoute.delete("/:id", authMiddleware, internController.delete);
-internRoute.get("/list", authMiddleware, internController.list);
+internRoute.get("/", authMiddleware, internController.list);
 internRoute.get("/:id", authMiddleware, internController.get);
 internRoute.put("/reset-password/:id", authMiddleware, internController.resetPassword);
 
