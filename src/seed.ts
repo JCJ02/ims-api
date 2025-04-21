f68a9538dcd5d8455a365cd9e0aad12686ebdb9a
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-22 01:50:38 +0800

    chore: refactor

--- src/seed.ts
+++ src/seed.ts
@@ -1,108 +1,48 @@
 import prisma from "./utils/prismaClient";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
-import dotenv from "dotenv";
 
-dotenv.config();
+// CREATE ADMIN METHOD
+async function createAdmin() {
 
-// CREATE A USER
-async function crateAdminUser() { 
+    try {
 
-    const hashPassword = bcrypt.hashSync("@lunas123", 10);
-    await prisma.admin.create({
-        data: {
-            firstname: "Danielle",
-            lastname: "Lunas",
-            email: 'lunas.danielle.10262002@gmail.com',
-            password: hashPassword,
-        },
-    });
+        const hashPassword = bcrypt.hashSync("password", 10);
 
-    console.log('Database Seeded Successfully.');
+        await prisma.$transaction(async (prisma) => {
+            await prisma.admin.create({
+                data: {
+                    firstname: "Danielle",
+                    lastname: "Lunas",
+                    email: 'lunas.danielle.10262002@gmail.com',
+                    account: {
+                        create: {
+                            password: hashPassword
+                        }
+                    }
+                },
+            });
+        });
+
+        console.log('Successfully Registered!.');
+
+    } catch (error) {
+
+        console.error("Error Seeding Database: ", error);
+
+    } finally {
+
+        await prisma.$disconnect();
+
+    }
+
 }
 
-
-// GET ALL USERS
-async function getAllAdminUserss() {
-
-    const users = await prisma.admin.findMany({
-        where: {
-            deletedAt: null
-        }
-    });
-
-    console.log(users);
-}
-
-
-// UPDATE ADMIN USER
-async function updateAdminUser() {
-
-    const hashPassword = bcrypt.hashSync("@lunas26", 10);
-
-    const updatedAdminUser = await prisma.admin.update({
-        where: {
-            id: 4
-        },
-        data: {
-            firstname: "Danielle Ladesma",
-            lastname: "Lunas",
-            email: 'lunas.danielle.10262002@gmail.com',
-            password: hashPassword,
-        }
-    })
-
-    console.log(updatedAdminUser);
-}
-
-
-// DELETE ADMIN USER 
-async function deleteAdminUser() {
-
-    const deletedAdminUser = await prisma.admin.update({
-        where: {
-            id: 4
-        },
-        data: {
-            deletedAt: new Date()
-        }
-    });
-
-    console.log(deletedAdminUser);
-}
-
-crateAdminUser()
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    })
-    .finally(async () => {
+createAdmin()
+    .then(async () => {
         await prisma.$disconnect();
-    });
-
-getAllAdminUserss()
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
     })
-    .finally(async () => {
+    .catch(async (error) => {
+        console.error("Error In Admin User Creation: ", error);
         await prisma.$disconnect();
-    });
-
-updateAdminUser()
-    .catch(error => {
-        console.error(error);
         process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
-
-deleteAdminUser()
-    .catch(error => {
-        console.error(error);
-        process.exit(1);
-    })
-    .finally(async () => {
-        await prisma.$disconnect();
     });
