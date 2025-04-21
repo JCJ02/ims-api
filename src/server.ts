69a604ddc4ff2b4f2edee54ba34fa7fbc62f8f80
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-22 01:33:23 +0800

    chore: refactor

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import bodyParser from "body-parser";
 
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.text({ type: 'text/html' }));
 app.use(cors());
 dotenv.config();
