5e875995c33542bcefcee6d3b196a03c31f78228
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-22 01:49:14 +0800

    feat: create verifyRefreshToken and generateRefereshToken function

--- src/utils/token.ts
+++ src/utils/token.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import jwt, { JsonWebTokenError, JwtPayload, TokenExpiredError } from "jsonwebtoken";
 
 const generateToken = (payload: any) => {
 
@@ -9,21 +9,60 @@ const generateToken = (payload: any) => {
     const token = jwt.sign(
         payload,
         process.env.JWT_SECRET_KEY,
-        { expiresIn: '1h' }
+        { expiresIn: '1d' }
     );
 
     return token;
 }
 
+const generateRefreshToken = (payload: any) => {
+
+    if (!process.env.JWT_REFRESH_SECRET_KEY) {
+        throw new Error("JWT_REFRESH_SECRET_KEY is not Defined in Environment Variables File!");
+    }
+
+    const refreshToken = jwt.sign(
+        payload,
+        process.env.JWT_REFRESH_SECRET_KEY,
+        { expiresIn: '7d' }
+    );
+
+    return refreshToken;
+
+}
+
 const verifyToken = (token: string) => {
 
     const verifiedToken = jwt.verify(token, process.env.JWT_SECRET_KEY as string);
-  
+
     return verifiedToken;
 
 };
 
+const verifyRefreshToken = (token: string): JwtPayload => {
+
+    if (!process.env.JWT_REFRESH_SECRET_KEY) {
+        throw new Error("JWT_REFRESH_SECRET_KEY is not Defined in Environment Variables File!");
+    }
+
+    try {
+        const verifiedRefreshToken = jwt.verify(token, process.env.JWT_REFRESH_SECRET_KEY) as JwtPayload;
+        return verifiedRefreshToken;
+    } catch (error) {
+        if (error instanceof JsonWebTokenError) {
+            throw new Error("Invalid Token");
+        }
+        if (error instanceof TokenExpiredError) {
+            throw new Error("Token Expired");
+        }
+        throw new Error("Token Verification Failed");
+    }
+
+}
+
 export {
     generateToken,
-    verifyToken
+    generateRefreshToken,
+    verifyToken,
+    verifyRefreshToken
 };
