e1a8085d117736a2e334e4f3c5b15816b6dd3dd1
John Carlo P. Jacobe <jacobe.johncarlo.02022003@gmail.com>
2025-04-22 01:47:37 +0800

    chore: refactor

--- src/services/AdminService.ts
+++ src/services/AdminService.ts
@@ -1,8 +1,8 @@
 
 import AdminRepository from "../repositories/AdminRepository";
 import { adminAccountType, adminType } from "../types/AdminType";
-import { sendEmails } from "../utils/sendInternAccountDetails";
-import { generateToken } from "../utils/token";
+import { sendInternAccountDetails } from "../utils/sendInternAccountDetails";
+import { generateRefreshToken, generateToken } from "../utils/token";
 import bcrypt from "bcryptjs";
 import { Request } from "express";
 
@@ -55,10 +55,32 @@ class AdminService {
 
         const token = generateToken({
             id: admin.id,
+            firstname: admin.firstname,
+            lastname: admin.lastname,
+            email: admin.email,
+            role: admin.role
+        });
+
+        const refreshToken = generateRefreshToken({
+            id: admin.id,
+            firstname: admin.firstname,
+            lastname: admin.lastname,
+            email: admin.email,
             role: admin.role
         });
         // console.log("Token: ", token);
-        return token;
+
+        return {
+            accessToken: token,
+            refreshToken: refreshToken,
+            admin: {
+                id: admin.id,
+                firstname: admin.firstname,
+                lastname: admin.lastname,
+                email: admin.email,
+                role: admin.role
+            }
+        };
 
     }
 
@@ -167,7 +189,7 @@ class AdminService {
             message: data.message
         };
 
-        return await sendEmails(options);
+        return await sendInternAccountDetails(options);
 
     }
 
